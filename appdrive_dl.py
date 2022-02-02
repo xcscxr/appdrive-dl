dcf011fe0c27fae36493a07c0f0316b1dc98ca6b
Yuuki <82380343+xcscxr@users.noreply.github.com>
2022-02-02 11:45:05 +0530

    U-A

--- appdrive_dl.py
+++ appdrive_dl.py
@@ -44,6 +44,7 @@ def appdrive_dl(url):
     
     headers = {
         "Content-Type": f"multipart/form-data; boundary={'-'*4}_",
+        "user-agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36"
     }
     
     data = {
